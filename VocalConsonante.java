351b6d31f952ad7a7a935ae4a4d5d81b85d3b717
L4KL4Kcoder <carloscapg2912@gmail.com>
2024-11-13 19:42:02 -0600

    Actualizacion con ciclo y eleccion

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -1,10 +1,89 @@
-public class VocalConsonante {
-    public static void main(String[] args) {
-        char ch = args[0].charAt(0);
+import  java.util.Scanner;
 
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " es una vocal.");
-        else 
-            System.out.println(ch + " es una consonante.");
+/*
+
+    @Autor: Carlos Palomar y Alejandra Sanchez
+
+*/
+
+public class VocalConsonante {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        String opcion;
+        String palabra;
+        char ch;
+        int vocal, consonante;
+        
+        do {            
+            
+            System.out.println("Que quieres saber: v o c");
+            opcion = in.nextLine();
+            System.out.println("dime tu palabra");
+            palabra = in.nextLine();
+            vocal = 0;
+            consonante = 0;
+            
+            switch (opcion) {
+                
+                case "v":
+                    
+                    for (int i = 0; i < palabra.length(); i++) {
+
+                        ch = palabra.charAt(i);
+
+                        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
+
+                            vocal ++;
+
+                        } else { 
+
+                            consonante ++;
+
+                        }
+
+                    }
+                    
+                    System.out.println("En la palabra " + palabra + " hay " + vocal + " vocales");
+                    
+                    break;
+                
+                case "c":
+                    
+                    for (int i = 0; i < palabra.length(); i++) {
+
+                        ch = palabra.charAt(i);
+
+                        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
+
+                            vocal ++;
+
+                        } else { 
+
+                            consonante ++;
+
+                        }
+
+                    }
+                    
+                    System.out.println("En la palabra " + palabra + " hay " + consonante + " consonantes");
+                    
+                    break;
+                
+                default:
+                    throw new AssertionError();
+            
+            }
+            
+            System.out.println("Para salir preciona q, para continuar preciona cualquier letra");
+            
+            opcion = in.nextLine();
+            
+        } while (!opcion.contains("q"));
+        
     }
+            
+    
 }
